042f7ba554aab7091ac0d9f0d1178e4aba89425f
hibatullah <ullahhibat@gmail.com>
2025-02-16 22:58:03 -0600

    Create a book endpoint, Author and Publisher get_by_id repo methods

--- server/app/bindings/repositories.py
+++ server/app/bindings/repositories.py
@@ -1,3 +1,7 @@
 def repositories(binder):
+    from app.Repositories.AuthorRepository import AuthorRepository
     from app.Repositories.BookRepository import BookRepository
+    from app.Repositories.PublisherRepository import PublisherRepository
+    binder.bind(AuthorRepository, to=AuthorRepository)
     binder.bind(BookRepository, to=BookRepository)
+    binder.bind(PublisherRepository, to=PublisherRepository)
